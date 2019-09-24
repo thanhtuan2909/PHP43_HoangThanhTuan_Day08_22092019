e046a64d275b5070a217aaa399f8fca97c778186
thanhtuan2909 <tuandhv96@gmail.com>
2019-09-24 18:46:04 +0700

    Bai 6 Day 8

---
+++ Bai6/js/myjs.js
@@ -0,0 +1,17 @@
+function handleSubmit() {
+	var str1 = document.getElementById('str1').value;
+	var str2 = document.getElementById('str2').value;
+	var check = str1.includes(str2);
+	if (str1 == '' || str2 == '') {
+		document.getElementById('error').innerHTML = "Các trường không được để trống";
+	} else {
+		if (check == true) {
+			document.getElementById('result').style = 'margin: 20px auto; color: green;';
+			document.getElementById('result').innerHTML = "Chuỗi " + "<b>" + str2 + "</b>" + " tồn tại trong chuỗi ban đầu " + "<b>" + str1 + "</b>";
+		} else {
+			document.getElementById('result').style = 'margin: 20px auto; color: blue;';
+			document.getElementById('result').innerHTML = "Chuỗi " + "<b>" + str2 + "</b>" + " không tồn tại trong chuỗi ban đầu " + "<b>" + str1 + "</b>";
+		}
+	}
+	return false;
+}
