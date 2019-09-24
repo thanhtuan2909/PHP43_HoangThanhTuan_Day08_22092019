2f85e876f2b59c9327ff22d28253a75ff0da6b8b
thanhtuan2909 <tuandhv96@gmail.com>
2019-09-24 21:11:24 +0700

    Bai 6 Day 8

--- Bai6/js/myjs.js
+++ Bai6/js/myjs.js
@@ -4,14 +4,14 @@ function handleSubmit() {
 	var check = str1.includes(str2);
 	if (str1 == '' || str2 == '') {
 		document.getElementById('error').innerHTML = "Các trường không được để trống";
-	} else {
-		if (check == true) {
+	} else if(check == true) {
+			document.getElementById('error').style = 'display: none;';
 			document.getElementById('result').style = 'margin: 20px auto; color: green;';
 			document.getElementById('result').innerHTML = "Chuỗi " + "<b>" + str2 + "</b>" + " tồn tại trong chuỗi ban đầu " + "<b>" + str1 + "</b>";
-		} else {
+	} else {
+			document.getElementById('error').style = 'display: none;';
 			document.getElementById('result').style = 'margin: 20px auto; color: blue;';
 			document.getElementById('result').innerHTML = "Chuỗi " + "<b>" + str2 + "</b>" + " không tồn tại trong chuỗi ban đầu " + "<b>" + str1 + "</b>";
-		}
 	}
 	return false;
 }
